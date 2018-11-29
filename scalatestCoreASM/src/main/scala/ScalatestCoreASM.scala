da89ae9e8d76774becc245d35736124321d4a620
André Wolski <locke@dena-design.de>
2018-11-29 16:02:00 +0100

    map input Files to canonicalFile..
    .. to avoid mixing relative and absolute Files.
    
    - compare via File.getParentFile.equals instead of null comparison
    - add basic logging
    - codestyle / more expressive variable names

--- scalatestCoreASM/src/main/scala/ScalatestCoreASM.scala
+++ scalatestCoreASM/src/main/scala/ScalatestCoreASM.scala
@@ -26,9 +26,10 @@ object ScalatestCoreASMPlugin extends AutoPlugin {
     sources := (sourceDirectory.value ** (includeFilter.value -- excludeFilter.value)).get,
 
     generateCoreASMTests := {
+      val log = streams.value.log
       val cachedFun = FileFunction.cached(streams.value.cacheDirectory, FilesInfo.lastModified, FilesInfo.lastModified) {
         (in: Set[File]) =>
-          Generator(sourceDirectory.value, in, sourceManaged.value) : Set[File]
+          Generator(sourceDirectory.value, in, sourceManaged.value, log) : Set[File]
       }
       cachedFun(sources.value.toSet).toSeq
     }
@@ -65,43 +66,61 @@ class %s extends TestAllCasm {
 }
 """
 
-  def apply(srcDir: File, files: Set[File], outDir: File): Set[File] = {
-    val m: Map[String, Set[File]] = toMap(srcDir, files)
+  def apply(srcDirIn: File, filesIn: Set[File], outDirIn: File, log: Logger): Set[File] = {
+    val srcDir: File = srcDirIn.getCanonicalFile
+    val files: Set[File] = filesIn.map(_.getCanonicalFile)
+    val outDir: File = outDirIn.getCanonicalFile
 
-    m.map(x => {
-        val parentFolder = x._1
+    log.info("ScalatestCoreASMPlugin: generating test files..")
+    log.debug("srcDir: " + srcDir)
+    log.debug("files: " + files)
+    log.debug("outDir: " + outDir)
+	
+    val testNamesWithFiles: Map[String, Set[File]] = toMap(srcDir, files)
+
+    log.debug("testNamesWithFiles: " + testNamesWithFiles)
+
+    val generatedFiles: Set[File] = testNamesWithFiles.map(x => {
+        val testName = x._1
         val files = x._2
-        generate(files, outDir, parentFolder)
+        generate(files, outDir, testName)
       }).toSet.flatten
+
+    log.debug("generatedFiles: " + generatedFiles)
+
+    generatedFiles
   }
 
   private def toMap(srcDir: File, files: Set[File]): Map[String, Set[File]] = {
-    val srcDirPath = srcDir.toPath
+    val filesWithTestName: Set[(File, String)] = files.map(f => {
+        val testName: String = if (f.getParentFile.equals(srcDir)) {
+          "WithoutTestClasses"
+        }
+        else {
+          f.getParentFile.getName
+        }
 
-    val x: Set[(String, File)] = files.map(f => {
-        val relativized: Path = srcDirPath.relativize(f.toPath)
-        val parent: Path = relativized.getParent
-        val x: String = if (parent == null) "WithoutTestClasses" else parent.getName(0).toString
-        (x, f)
+        (f, testName)
       })
 
-    val keys: Set[String] = x.map(_._1)
-    val m: Map[String, Set[File]] = keys.map(key => {
-        val files: Set[File] = x.filter(_._1 == key).map(_._2)
+    val testNames: Set[String] = filesWithTestName.map(_._2)
+
+    val testNamesWithFiles: Map[String, Set[File]] = testNames.map(key => {
+        val files: Set[File] = filesWithTestName.filter(_._2 == key).map(_._1)
         (key, files)
       }).toMap
 
-    m
+    testNamesWithFiles
   }
 
-  private def generate(files: Set[File], outDir: File, parentFolder: String): Set[File] = {
+  private def generate(files: Set[File], outDir: File, testName: String): Set[File] = {
     val filesAbsolute = files.map(f => "new JFile(\"\"\""+f.getCanonicalPath+"\"\"\")")
 
     val filesSeq = filesAbsolute.mkString("Seq(", ", ", ").map(_.toPath)")
 
-    val source = template format (info, "Test" + parentFolder, filesSeq)
+    val source = template format (info, "Test" + testName, filesSeq)
 
-    val outFile = outDir / ("Test" + parentFolder + ".scala")
+    val outFile = outDir / ("Test" + testName + ".scala")
     IO.write(outFile, source)
 
     Set(outFile)
